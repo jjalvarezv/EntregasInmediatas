9854101d773c8a7c8169b31933151beddff813e5
jjhurtadoa <jjhurtadoa@unal.edu.co>
2019-06-20 23:04:03 -0500

    gestion
    gestorAplicacion.gestion defincion de metodos y atributos, de todos los metodos de este paquete, se implementan algunos.

--- src/gestorAplicacion/gestion/Item.java
+++ src/gestorAplicacion/gestion/Item.java
@@ -1,5 +1,32 @@
 package gestorAplicacion.gestion;
 
 public class Item {
+	private int costo, cantidad, total;	
+	private Producto producto;
+	
+	public Item() {
+		
+	}
+	public Item(int costo, int cantidad, Producto prod, int total) {
+		this.costo= costo;
+		this.cantidad= cantidad;
+		this.producto= prod;
+		this.total= total;
+	}
+	public void  calcularTotal() {
+		
+	}
+	public int getCosto() {
+		return this.costo;
+	}
+	public int getCantidad() {
+		return this.cantidad;
+	}
+	public int getTotal() {
+		return this.total;
+	}
+	public Producto getProducto() {
+		return this.producto;
+	}
 
 }
--- src/gestorAplicacion/gestion/Producto.java
+++ src/gestorAplicacion/gestion/Producto.java
@@ -2,4 +2,31 @@ package gestorAplicacion.gestion;
 
 public class Producto {
 
+	private Restaurante restaurante;
+	private String nombre_producto;
+	private int  valor_unidadad ;
+	private int tiempo_de_preparacion;
+	
+	public Producto(Restaurante rest, String nombre, int valor, int tiempo) {
+		this.restaurante= rest;
+		this.nombre_producto= nombre;
+		this.valor_unidadad= valor;
+		this.tiempo_de_preparacion= tiempo;
+	}
+	public void  setValorUnidad(int nuevo_valor) {
+		this.valor_unidadad= nuevo_valor;
+	}
+	public Restaurante   getRestaurante() {
+		return this.restaurante;
+				
+	}
+	public String getNombreProducto() {
+		return this.nombre_producto;
+	}
+	public int getValorUnidad() {
+		return this.valor_unidadad;
+	}
+	public int getTiempoDePreparacion() {
+		return this.tiempo_de_preparacion;
+	}
 }
