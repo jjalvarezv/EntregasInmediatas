f131ca71191451f3b3ed3d41b7b23a15fe1e4111
jjhurtadoa <jjhurtadoa@unal.edu.co>
2019-07-17 12:12:23 -0500

    usuario invitado
    registrarCliente

--- src/gestorAplicacion/personas/UsuarioInvitado.java
+++ src/gestorAplicacion/personas/UsuarioInvitado.java
@@ -1,5 +1,10 @@
 package gestorAplicacion.personas;
 
 public class UsuarioInvitado {
+	public void ResgistrarseCliente(String userName, String pass, String nombre, Long cc, String direccion) {
+		//String userName, String pass, String nombre, Long cc, String direccion
+		Cliente nuevoCliente = new Cliente(userName, pass, nombre, cc, direccion);
+	}
+	
 
 }
