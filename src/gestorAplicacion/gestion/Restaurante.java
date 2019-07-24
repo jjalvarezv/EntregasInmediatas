edab81f3c69c9b4f8cda8880188e4a682c223ac9
jjhurtadoa <jjhurtadoa@unal.edu.co>
2019-07-23 22:48:08 -0500

    Cambiamos de todo
    clase REstaurante, Item, Propietario REstaurante, MenuConsola
    
    Co-Authored-By: stng1105 <stng1105@users.noreply.github.com>

--- src/gestorAplicacion/gestion/Restaurante.java
+++ src/gestorAplicacion/gestion/Restaurante.java
@@ -7,25 +7,33 @@ public class Restaurante implements CalcularGanancias{
 
 	private int codigo;
 	private String direccion;
-	private PropietarioRestaurante propietario;
-	private long telefono;
+	//private PropietarioRestaurante propietario;
+	private long telefono, cedulaPropietario;
 	private ArrayList<Producto> Carta;
-	public Restaurante palitos= new Restaurante(1234, "calle 98 #30-20", propietario.jorge, (long)4452347);
+	//public Restaurante palitos= new Restaurante(1234, "calle 98 #30-20", propietario.jorge, (long)4452347);
 	
-	public Restaurante(int cod, String direcc, PropietarioRestaurante prop, long tel) {
+	public Restaurante(int cod, String direcc, long cedulaPropietario, long tel) {
 		this.codigo= cod;
 		this.direccion= direcc;
-		this.propietario= prop;
+		this.cedulaPropietario= cedulaPropietario;
 		this.telefono= tel;
 	}
 	public Restaurante(int cod, String direcc, long tel){
-		this(cod, direcc,null,tel);
+		this.codigo= cod;
+		this.direccion= direcc;
+		//this.cedulaPropietario= cedulaPropietario;
+		this.telefono= tel;
+		
 	}
-	public void agregarProducto(Producto producto) {
-		Carta.add(producto);
+	public void agregarProducto(Producto producto, long CedulaPropietario) {
+		if (CedulaPropietario == this.cedulaPropietario) {
+			Carta.add(producto);
+		}		
 	}
-	public void eliminarProducto(Producto producto) {
-		Carta.remove(producto);
+	public void eliminarProducto(Producto producto, long cedulaPropietario) {
+		if (cedulaPropietario == this.cedulaPropietario) {
+		 Carta.remove(producto);
+		}
 	}
 	@Override
 	public int calcular_Ganancias() {
@@ -43,9 +51,16 @@ public class Restaurante implements CalcularGanancias{
 	 public String getDireccion() {
 		 return this.direccion;
 	 } 
-	 public PropietarioRestaurante getPropietario() {
-		 return this.propietario;
+	 public long getCedulaPropietario() {
+		 return this.cedulaPropietario;
 	 }
+	 public void setCedulaPropietario(long cc) {
+		
+			 this.cedulaPropietario = cc;
+	
+			 
+	 }
+	
 	 public long getTelefono() {
 		 return this.telefono;
 	 }
