c2bc0107dd087ec4fc431bc5eb7710cf14d3ef17
jjalvarezv <32003543+jjalvarezv@users.noreply.github.com>
2019-08-27 21:19:21 -0500

    correccion en infoUser
    Co-Authored-By: stng1105 <stng1105@users.noreply.github.com>

--- src/controlador/infoUser.java
+++ src/controlador/infoUser.java
@@ -15,8 +15,8 @@ public class infoUser implements ActionListener{
 
 		login lg = new login();
 		UsuarioRegistrado usuarioActual = lg.userSession;
-		Long cc = usuarioActual.getCedula();
-		String nombre = usuarioActual.getNom();
+		long cc;
+		String nombre; 
 		String dir;
 		int cod;
 		int estado;
@@ -24,12 +24,16 @@ public class infoUser implements ActionListener{
 		//El if es para hacer el cast a la instancia del objeto y asi acceder a los atributos de los usuarios
 		if (usuarioActual instanceof Cliente) {
 			dir = ((Cliente) usuarioActual).getDireccion();
+			nombre = ((Cliente) usuarioActual).getNom();
+			cc = ((Cliente) usuarioActual).getCedula();
 			JOptionPane.showMessageDialog(null,"Nombre: "+nombre+"\nCedula: "+cc+
 					"\nDirección: "+dir
 					,"Información de Cliente", JOptionPane.INFORMATION_MESSAGE);
 			
 		}else if(usuarioActual instanceof PropietarioRestaurante) {
 			cod = ((PropietarioRestaurante) usuarioActual).getCodigo();
+			nombre = ((PropietarioRestaurante) usuarioActual).getNom();
+			cc = ((PropietarioRestaurante) usuarioActual).getCedula();
 			JOptionPane.showMessageDialog(null,"Nombre: "+nombre+"\nCedula: "+cc+
 					"\nCodigo de Restaurante: "+cod
 					,"Información de Propietario", JOptionPane.INFORMATION_MESSAGE);
@@ -44,11 +48,15 @@ public class infoUser implements ActionListener{
 			}else {
 				est = "OFF-LINE";
 			}
+			nombre = ((Domiciliario)usuarioActual).getNom();
+			cc = ((Domiciliario)usuarioActual).getCedula();
 			JOptionPane.showMessageDialog(null,"Nombre: "+nombre+"\nCedula: "+cc+
 					"\nEstado: "+est
 					,"Información de Domiciliario", JOptionPane.INFORMATION_MESSAGE);
 			
 		}else if (usuarioActual instanceof Admin) {
+			nombre = ((Admin)usuarioActual).getNom();
+			cc = ((Admin)usuarioActual).getCedula();
 			JOptionPane.showMessageDialog(null,"Nombre: "+nombre+"\nCedula: "+cc
 					,"Información de Administrador", JOptionPane.INFORMATION_MESSAGE);
 		}
