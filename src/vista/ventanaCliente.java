9206df29aae25377ab50e25ec7297d0e661979ea
jjhurtadoa <jjhurtadoa@unal.edu.co>
2019-08-28 00:44:21 -0500

    solicitar pedido
    Co-Authored-By: daramirez97 <daramirez97@users.noreply.github.com>
    Co-Authored-By: jjalvarezv <jjalvarezv@users.noreply.github.com>

--- src/vista/ventanaCliente.java
+++ src/vista/ventanaCliente.java
@@ -1,7 +1,25 @@
 package vista;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ventanaCliente extends JPanel{
-
+	
+	JPanel panelAux;
+	
+	public ventanaCliente() {
+		
+	}
+	
+	public ventanaCliente(String usuario) {
+		
+		
+		this.setLayout(new BorderLayout());
+		add(new JLabel("HOLAAAAA "+usuario), BorderLayout.NORTH);
+		add(new JLabel("mierasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddda"), BorderLayout.WEST);
+		add(new JLabel("mierda2"), BorderLayout.EAST);
+		
+	}
 }
+	
+
--- src/vista/ventanaSolicitarPedido.java
+++ src/vista/ventanaSolicitarPedido.java
@@ -1,11 +1,30 @@
 package vista;
 
+import java.awt.BorderLayout;
+import java.awt.*;
+
+import javax.swing.*;
 import javax.swing.JPanel;
 
 public class ventanaSolicitarPedido extends JPanel{
 	
-	public ventanaSolicitarPedido() {
+	public static JPanel panelAux;
 		
+	public ventanaSolicitarPedido() {	}
+		
+	public ventanaSolicitarPedido(String usuario) {
+			
+		this.setLayout(new BorderLayout());
+		add(new JLabel("HOLAAAAA "+usuario), BorderLayout.NORTH);
+		panelAux = new JPanel();
+		panelAux.setLayout(new GridLayout(1,2,10,5));
+		panelAux.add(new JButton("mierda1"));
+		panelAux.add(new JButton("mierda2"));
+		add(panelAux, BorderLayout.CENTER);
+		
+			
 	}
-
+		
 }
+
+
